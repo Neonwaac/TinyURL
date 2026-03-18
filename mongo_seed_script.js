5500855f8ae29e8df964b3759f6ca2bcaaebba31
neonwaac <jomaparo3103@gmail.com>
2026-03-17 22:02:56 -0500

    real images added

--- mongo_seed_script.js
+++ mongo_seed_script.js
@@ -1,14 +1,20 @@
 db.links_metadata.insertMany([
   {
     _id: "Ab12Xy",
-    imagen: "https://via.placeholder.com/150",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c1/Google_%22G%22_logo.svg/3840px-Google_%22G%22_logo.svg.png",
     descripcion: "Buscador Google",
     createdAt: new Date()
   },
   {
     _id: "Zx98Qr",
-    imagen: "https://via.placeholder.com/150",
+    imagen: "https://upload.wikimedia.org/wikipedia/commons/e/ef/Youtube_logo.png",
     descripcion: "Plataforma de videos",
+    createdAt: new Date()
+  },
+    {
+    _id: "Lm45No",
+    imagen: "https://images.icon-icons.com/3685/PNG/512/github_logo_icon_229278.png",
+    descripcion: "Repositorio de código",
     createdAt: new Date()
   }
 ]);
