75dcc780520cfeafac67800a800ab7acf7ec7520
neonwaac <jomaparo3103@gmail.com>
2026-03-17 21:52:19 -0500

    database

---
+++ mongo_seed_script.js
@@ -0,0 +1,14 @@
+db.links_metadata.insertMany([
+  {
+    _id: "Ab12Xy",
+    imagen: "https://via.placeholder.com/150",
+    descripcion: "Buscador Google",
+    createdAt: new Date()
+  },
+  {
+    _id: "Zx98Qr",
+    imagen: "https://via.placeholder.com/150",
+    descripcion: "Plataforma de videos",
+    createdAt: new Date()
+  }
+]);
